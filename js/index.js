fcbd06a97cd9f182185e856969cf6339c29102c8
Vladislav Zhurakosvkiy <dkfl123@bigmir.net>
2016-09-13 18:40:29 +0300

    push

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('#save-btn').on("click", function () {
+	$('.popup-save').addClass('visibility');
+});
+$('#popup-save__close').on("click", function () {
+	$('.popup-save').removeClass('visibility');
+});
+$('#registr').on("click", function () {
+	$('.reg').addClass('visibility');
+});
+$('#reg__close').on("click", function () {
+	$('.reg').removeClass('visibility');
+});
